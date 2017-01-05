c54fa482179fe85d2aefc6f850e33813ade2eaab
Sanqui <gsanky@gmail.com>
2017-01-05 17:04:55 +0100

    Skip empty rows for real when importing

--- import.py
+++ import.py
@@ -14,7 +14,7 @@ db.session.add(category)
 with open(path) as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        if not row: continue
+        if not row[0]: continue
         suggestions = []
         comments = []
         if len(row) == 3:
